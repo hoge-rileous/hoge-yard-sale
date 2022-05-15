38917cc40109f4f285b9daa20fa0dc562fb9932a
Nick <nick@skayl.com>
2022-05-15 19:09:17 -0400

    Split initialize to initialize1 / initialize2

--- contracts/VendorFactory.sol
+++ contracts/VendorFactory.sol
@@ -8,22 +8,27 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./TokenVendor.sol";
 
 interface ITokenVendor {
-    function initialize(uint buyPrice, uint sellPrice, address tokenAddress) external;
+    function initialize1(address tokenAddress) external;
+    function initialize2(uint buyPrice, uint sellPrice) external;
 }
 
 contract VendorFactory is Context {
 
     event VendorCreated(address indexed creator, address indexed vendor);
     address vendorContract;
+    mapping (address => address) proxiedVendor;
 
     constructor(address vendorAddress) {
         vendorContract = vendorAddress;
+        address hogeVendor = Clones.clone(vendorContract);
+        ITokenVendor(hogeVendor).initialize1(0xfAd45E47083e4607302aa43c65fB3106F1cd7607);
+        proxiedVendor[0xfAd45E47083e4607302aa43c65fB3106F1cd7607] = hogeVendor;
     }
 
     function createVendor(uint buyPrice, uint sellPrice, address tokenAddress) public payable returns (address new_vendor) {
         //require(buyPrice >= sellPrice, "buyPrice must be larger than sellPrice")
-        new_vendor = Clones.clone(vendorContract);
-        ITokenVendor(new_vendor).initialize(buyPrice, sellPrice, tokenAddress);
+        new_vendor = Clones.clone(proxiedVendor[tokenAddress]);
+        ITokenVendor(new_vendor).initialize2(buyPrice, sellPrice);
         Ownable(new_vendor).transferOwnership(_msgSender());
         payable(new_vendor).transfer(msg.value);
         emit VendorCreated(_msgSender(), new_vendor);
